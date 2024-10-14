9eac8e61cd49fdf64031fd0a8e2ea3579e4e4845
Craig <craigk00@hotmail.com>
2024-10-14 09:30:07 -0400

    enemy AI base

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -11,13 +11,30 @@ public class Enemy : MonoBehaviour
     float visionAngle = 30f;
 
     [SerializeField, Range(1, 5)]
-    int precision = 1;
+    int precision = 2;
+
+    [SerializeField, Range(1f, 5f)]
+    int maxSpeed = 2;
+
+    [SerializeField, Range(0.001f, 1f)]
+    int moveDrag = 2;
     Transform fDot;
+    int hitLeft = 0;
+    int hitRight = 0;
+
+    public Vector3 targetPos;
+    public bool hasTarget = false;
+    public int preferedTurnDir = 1;
+
+    Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Awake()
     {
         fDot = transform.Find("ForwardDot");
+
+        preferedTurnDir = (int)Mathf.Sign(Random.Range(-1f, 1f));
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -25,7 +42,11 @@ public class Enemy : MonoBehaviour
 
     private void FixedUpdate()
     {
-        VisionCast();
+        if (hasTarget)
+        {
+            VisionCast();
+            Move();
+        }
     }
 
     void VisionCast()
@@ -34,15 +55,49 @@ public class Enemy : MonoBehaviour
         bool hit;
         Vector3 forward;
         float deltaAngle;
-        for (int i = 0; i <= 2 * precision + 1; i++)
+        // left
+        for (int i = 0; i <= precision; i++)
         {
-            // hit = Physics2D.Raycast(transform.position)
             forward = (fDot.position - transform.position).normalized;
-            // left to right
             deltaAngle = Mathf.Lerp(visionAngle, -visionAngle, (float)i / (precision * 2 + 1));
             forward = forward.Rotate(deltaAngle);
             Debug.DrawRay(transform.position, forward * visionDistance, Color.green);
-            // Debug.Log(forward);
+            hit = Physics2D.Raycast(transform.position, forward, visionDistance, 6);
+            if (hit)
+                hitLeft++;
         }
+
+        // right
+        for (int i = precision + 1; i <= 2 * precision + 1; i++)
+        {
+            forward = (fDot.position - transform.position).normalized;
+            deltaAngle = Mathf.Lerp(visionAngle, -visionAngle, (float)i / (precision * 2 + 1));
+            forward = forward.Rotate(deltaAngle);
+            Debug.DrawRay(transform.position, forward * visionDistance, Color.red);
+            hit = Physics2D.Raycast(transform.position, forward, visionDistance, 6);
+            if (hit)
+                hitRight++;
+        }
+    }
+
+    void Move()
+    {
+        if (hitLeft == hitRight && hitLeft > 0)
+        {
+            // turn to a side
+            transform.Rotate(0, 0, 1f * preferedTurnDir);
+        }
+        else if (hitLeft > hitRight && hitLeft > 1)
+        {
+            // turn right
+            transform.Rotate(0, 0, 1f);
+        }
+        else if (hitRight > hitLeft && hitRight > 1)
+        {
+            // turn left
+            transform.Rotate(0, 0, -1f);
+        }
+
+        rb.velocity = (fDot.position - transform.position) * maxSpeed;
     }
 }
