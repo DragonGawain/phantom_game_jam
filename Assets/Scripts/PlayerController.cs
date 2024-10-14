9496d9c0275a79428b2383950f2cb54031095db6
Craig <craigk00@hotmail.com>
2024-10-13 21:57:13 -0400

    basic character controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Inputs inputs;
+    Vector2 movementInput;
+
+    Rigidbody2D rb;
+
+    [SerializeField, Range(1f, 5f)]
+    float maxMoveSpeed = 2f;
+
+    [SerializeField, Range(0f, 5f)]
+    float slowdownDrag = 0.2f;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        //
+        rb = GetComponent<Rigidbody2D>();
+        inputs = new Inputs();
+        inputs.Player.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
         //
+    }
+
+    private void FixedUpdate()
+    {
+        movementInput = inputs.Player.Move.ReadValue<Vector2>().normalized;
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity + movementInput, maxMoveSpeed);
+        if (rb.velocity.magnitude > 0)
+            rb.velocity -= rb.velocity.normalized * slowdownDrag;
     }
 }
