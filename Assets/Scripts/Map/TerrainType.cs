d2a909eb627ca94e7552f7ed43a14e98f0736480
DianaLac <diana@lacraru.com>
2024-10-14 18:19:15 -0400

    Edit TerrainTypes

--- Assets/Scripts/Map/TerrainType.cs
+++ Assets/Scripts/Map/TerrainType.cs
@@ -36,6 +36,27 @@ public class TerrainType : MonoBehaviour
             case TerrainTypes.ASPHALT:
                 movement.SetMaxMoveSpeed(movement.GetOriginalSpeed() + 1);
                 break;
+            case TerrainTypes.FOREST:
+                if (go.TryGetComponent<PlayerController>(out PlayerController pc)) // returns a bool value
+                {
+                    go.transform.Find("Flashlight").gameObject.SetActive(true);  // activate light
+                    GameObject.FindGameObjectWithTag("GlobalLight").SetActive(false);
+                }
+                break;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        GameObject go = other.gameObject; // what game object collided?
+        Movement movement = go.GetComponent<Movement>();
+        if (terrainType == TerrainTypes.FOREST)
+        {
+            if(go.TryGetComponent<PlayerController>(out PlayerController pc))
+            {
+                go.transform.Find("Flashlight").gameObject.SetActive(false);  
+                GameObject.FindGameObjectWithTag("GlobalLight").SetActive(true);
+            }
         }
     }
 }
