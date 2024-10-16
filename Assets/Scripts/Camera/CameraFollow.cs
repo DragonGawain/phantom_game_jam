f38a9af449d0116f3a0b7d3a3f5e760760b04c8a
DianaLac <diana@lacraru.com>
2024-10-16 12:09:51 -0400

    Camera Follow

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,24 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform followTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // find player
+        followTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //follows player
+        transform.position = new Vector3(followTransform.position.x, followTransform.position.y, transform.position.z);
+    }
+}
