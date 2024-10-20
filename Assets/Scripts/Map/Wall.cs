54a95a3254537c4422316c9d032e66c645fa4e93
DragonGawain <craigk00@hotmail.com>
2024-10-20 15:45:32 -0400

    mini cleanup

--- Assets/Scripts/Map/Wall.cs
+++ Assets/Scripts/Map/Wall.cs
@@ -6,17 +6,7 @@ using UnityEngine;
 public class Wall : MonoBehaviour
 {
     [SerializeField] bool checkBullet;
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         
